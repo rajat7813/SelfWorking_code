5d4e14aaddeece14ca5b960accd1add0d5e29aea
ARORA Rajat <raarora@one.steria.dom>
2018-04-13 15:19:41 +0530

    office code

--- CMS_Xml_Stub/src/main/java/org/epo/dao/CmsDao.java
+++
@@ -1,9 +0,0 @@
-package org.epo.dao;
-
-import org.epo.exception.CustomException;
-import org.epo.readDossier.DossierInfo;
-
-public interface CmsDao {
-	String insertDossierInfo(DossierInfo dossierInfo) throws CustomException;
-	String getDocMasterData() throws CustomException;
-}
--- epo-mock/src/main/java/com/edyt/mock/domain/deputisation/AccountRoleStatusCode.java
+++
@@ -1,8 +0,0 @@
-package com.edyt.mock.domain.deputisation;
-
-
-public enum AccountRoleStatusCode {
-    AM_ASS_Accepted,
-    AM_ASS_Revoked,
-    AM_ASS_Rejected
-}
